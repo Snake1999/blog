f8193e384d916d099671ef7e2e1d644e7bc2bd95
Lin Mulan <admin@snake1999.com>
2016-12-03 09:51:05 +0800

    Site updated: 2016-12-03 09:51:04

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,5 +1,3 @@
-$(document).ready(function () {
-  var sidebarTop = $('.header-inner').height() + 10;
-
-  $('#sidebar').css({ 'margin-top': sidebarTop }).show();
-});
+// build time:Sat Dec 03 2016 09:50:36 GMT+0800 (中国标准时间)
+$(document).ready(function(){var e=$(".header-inner").height()+10;$("#sidebar").css({"margin-top":e}).show()});
+//rebuild by neat 
